ca67771d68d84f3febb3b2dc557472318c870584
Andre Martins <asiggi01@dcs.bbk.ac.uk>
2021-05-21 17:30:29 +0100

    add table diagram

---
+++ diagrams/create-tables.sql
@@ -0,0 +1,215 @@
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "surname" varchar,
+  "birthday" date,
+  "post_code" varchar,
+  "city" varchar,
+  "region" varchar,
+  "country" varchar,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "acts" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "act_users" (
+  "act_id" int,
+  "user_id" int,
+  "user_role_name" varchar,
+  PRIMARY KEY ("act_id", "user_id")
+);
+
+CREATE TABLE "payment_vendors" (
+  "name" varchar PRIMARY KEY,
+  "title" varchar
+);
+
+CREATE TABLE "user_payment_methods" (
+  "id" SERIAL,
+  "user_id" int,
+  "title" varchar UNIQUE,
+  "vendor" varchar,
+  "metadata" jsonb,
+  PRIMARY KEY ("id", "user_id")
+);
+
+CREATE TABLE "studios" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar,
+  "description" varchar,
+  "address_1" varchar,
+  "address_2" varchar,
+  "address_3" varchar,
+  "post_code" varchar,
+  "city" varchar,
+  "region" varchar,
+  "country" varchar,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "studio_users" (
+  "studio_id" int,
+  "user_id" int,
+  "studio_role_name" varchar,
+  PRIMARY KEY ("studio_id", "user_id")
+);
+
+CREATE TABLE "spaces" (
+  "id" SERIAL PRIMARY KEY,
+  "studio_id" int,
+  "title" varchar,
+  "description" varchar,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "space_users" (
+  "space_id" int,
+  "user_id" int,
+  "space_role_name" varchar,
+  PRIMARY KEY ("space_id", "user_id")
+);
+
+CREATE TABLE "services" (
+  "type" varchar PRIMARY KEY,
+  "title" varchar
+);
+
+CREATE TABLE "studio_services" (
+  "id" SERIAL PRIMARY KEY,
+  "studio_id" int,
+  "service_type" varchar,
+  "title" varchar,
+  "price" money
+);
+
+CREATE TABLE "space_services" (
+  "id" SERIAL PRIMARY KEY,
+  "space_id" int,
+  "service_type" varchar,
+  "title" varchar,
+  "price" money
+);
+
+CREATE TABLE "bookings" (
+  "id" SERIAL PRIMARY KEY,
+  "studio_id" int,
+  "user_id" int,
+  "act_id" int,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "booking_payments" (
+  "id" SERIAL PRIMARY KEY,
+  "booking_id" int,
+  "user_id" int,
+  "vendor" varchar,
+  "metadata" jsonb,
+  "value" money,
+  "is_paid" boolean,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "booking_items" (
+  "booking_id" int,
+  "space_id" int,
+  "service_type" varchar,
+  "service_title" varchar,
+  "service_price" money,
+  "start_at" timestamp,
+  "end_at" timestamp,
+  "quantity" int,
+  PRIMARY KEY ("booking_id", "space_id")
+);
+
+CREATE TABLE "booking_reviews" (
+  "booking_id" int,
+  "space_id" int,
+  "title" varchar,
+  "description" varchar,
+  "stars" int,
+  "created_at" timestamp DEFAULT (now()),
+  PRIMARY KEY ("booking_id", "space_id")
+);
+
+CREATE TABLE "studio_roles" (
+  "name" varchar PRIMARY KEY,
+  "title" varchar,
+  "permissions" jsonb
+);
+
+CREATE TABLE "space_roles" (
+  "name" varchar PRIMARY KEY,
+  "title" varchar,
+  "permissions" jsonb
+);
+
+CREATE TABLE "user_roles" (
+  "name" varchar PRIMARY KEY,
+  "title" varchar,
+  "permissions" jsonb
+);
+
+ALTER TABLE "act_users" ADD FOREIGN KEY ("act_id") REFERENCES "acts" ("id");
+
+ALTER TABLE "act_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "act_users" ADD FOREIGN KEY ("user_role_name") REFERENCES "user_roles" ("name");
+
+ALTER TABLE "user_payment_methods" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "user_payment_methods" ADD FOREIGN KEY ("vendor") REFERENCES "payment_vendors" ("name");
+
+ALTER TABLE "studio_users" ADD FOREIGN KEY ("studio_id") REFERENCES "studios" ("id");
+
+ALTER TABLE "studio_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "studio_users" ADD FOREIGN KEY ("studio_role_name") REFERENCES "studio_roles" ("name");
+
+ALTER TABLE "spaces" ADD FOREIGN KEY ("studio_id") REFERENCES "studios" ("id");
+
+ALTER TABLE "space_users" ADD FOREIGN KEY ("space_id") REFERENCES "spaces" ("id");
+
+ALTER TABLE "space_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "space_users" ADD FOREIGN KEY ("space_role_name") REFERENCES "space_roles" ("name");
+
+ALTER TABLE "studio_services" ADD FOREIGN KEY ("studio_id") REFERENCES "studios" ("id");
+
+ALTER TABLE "studio_services" ADD FOREIGN KEY ("service_type") REFERENCES "services" ("type");
+
+ALTER TABLE "space_services" ADD FOREIGN KEY ("space_id") REFERENCES "spaces" ("id");
+
+ALTER TABLE "space_services" ADD FOREIGN KEY ("service_type") REFERENCES "services" ("type");
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("studio_id") REFERENCES "studios" ("id");
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("act_id") REFERENCES "acts" ("id");
+
+ALTER TABLE "booking_payments" ADD FOREIGN KEY ("booking_id") REFERENCES "bookings" ("id");
+
+ALTER TABLE "booking_payments" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "booking_payments" ADD FOREIGN KEY ("vendor") REFERENCES "payment_vendors" ("name");
+
+ALTER TABLE "booking_items" ADD FOREIGN KEY ("booking_id") REFERENCES "bookings" ("id");
+
+ALTER TABLE "booking_items" ADD FOREIGN KEY ("space_id") REFERENCES "spaces" ("id");
+
+ALTER TABLE "booking_items" ADD FOREIGN KEY ("service_type") REFERENCES "services" ("type");
+
+ALTER TABLE "booking_reviews" ADD FOREIGN KEY ("booking_id") REFERENCES "bookings" ("id");
+
+ALTER TABLE "booking_reviews" ADD FOREIGN KEY ("space_id") REFERENCES "spaces" ("id");
+
+
+COMMENT ON COLUMN "studio_roles"."permissions" IS 'array';
+
+COMMENT ON COLUMN "space_roles"."permissions" IS 'array';
+
+COMMENT ON COLUMN "user_roles"."permissions" IS 'array';
