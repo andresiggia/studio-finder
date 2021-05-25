41953f656c247e859ca98889fd38f4a914edc8b5
Andre Martins <asiggi01@dcs.bbk.ac.uk>
2021-05-25 11:13:16 +0100

    add context to Home

--- studio-finder/src/pages/Home.tsx
+++ studio-finder/src/pages/Home.tsx
@@ -1,25 +1,37 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+// context
+import AppContext from '../context/AppContext';
+
+// components
 import ExploreContainer from '../components/ExploreContainer';
+
+// css
 import './Home.css';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+class Home extends React.Component {
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Blank</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <ExploreContainer />
+        </IonContent>
+      </IonPage>
+    );
+  }
 };
+
+Home.contextType = AppContext;
 
 export default Home;
