982d6c0d40cee80754757ef2eff174f1d630fcbc
Andre Martins <asiggi01@dcs.bbk.ac.uk>
2021-06-10 19:34:35 +0100

    update header to include main links

--- studio-finder/src/components/Header/Header.tsx
+++ studio-finder/src/components/Header/Header.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
-import { IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonHeader, IonTitle, IonToolbar, IonButtons, IonButton,
+} from '@ionic/react';
+
+import i18n from '../../services/i18n/i18n';
 
 class Header extends React.Component {
   render() {
     return (
       <IonHeader>
+
         <IonToolbar>
-          <IonTitle>
+          <IonTitle slot="start">
             Studio
             <strong>Finder</strong>
           </IonTitle>
+
+          <IonButtons>
+            <IonButton fill="clear" color="primary">
+              {i18n.t('Home')}
+            </IonButton>
+            <IonButton fill="clear" color="primary">
+              {i18n.t('About')}
+            </IonButton>
+            <IonButton fill="clear" color="primary">
+              {i18n.t('Studio Login')}
+            </IonButton>
+          </IonButtons>
+
+          <IonButtons slot="end">
+            <IonButton fill="outline" color="primary">
+              {i18n.t('Log In')}
+            </IonButton>
+            <IonButton fill="solid" color="primary">
+              {i18n.t('Sign Up')}
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
+
       </IonHeader>
     );
   }
