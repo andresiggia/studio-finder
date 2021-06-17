12962e1c52cb78c8d6f02205e4dc2bbcad6614cf
Andre Martins <asiggi01@dcs.bbk.ac.uk>
2021-06-17 20:14:51 +0100

    add popup to login

--- studio-finder/src/pages/Login/Login.tsx
+++ studio-finder/src/pages/Login/Login.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import {
+  IonContent, IonPage, IonModal, IonTitle, IonToolbar, IonIcon, IonButton, IonButtons,
+} from '@ionic/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { closeOutline } from 'ionicons/icons';
 
 // context
 import AppContext from '../../context/AppContext';
@@ -7,16 +12,68 @@ import AppContext from '../../context/AppContext';
 // components
 import Header from '../../components/Header/Header';
 
+// services
+import i18n from '../../services/i18n/i18n';
+
 // css
 import './Login.css';
 
-class Login extends React.Component {
+interface State {
+  isOpen: boolean
+}
+
+class Login extends React.Component<RouteComponentProps, State> {
+  constructor(props: RouteComponentProps) {
+    super(props);
+    this.state = {
+      isOpen: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      isOpen: true,
+    });
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      isOpen: false,
+    });
+  }
+
+  onClose = () => {
+    const { location, history } = this.props;
+    const query = new URLSearchParams(location.search);
+    const backUrl = query.get('backUrl') || '/';
+    history.push(backUrl);
+  }
+
   render() {
+    const { isOpen } = this.state;
     return (
       <IonPage>
         <IonContent fullscreen>
           <Header />
-          <p>Login - Under development</p>
+          <IonModal
+            isOpen={isOpen}
+            backdropDismiss={false}
+          >
+            <IonToolbar>
+              <IonTitle slot="start" className="header-title">
+                Login
+              </IonTitle>
+              <IonButtons slot="end">
+                <IonButton
+                  color="primary"
+                  onClick={this.onClose}
+                >
+                  <IonIcon icon={closeOutline} ariaLabel={i18n.t('Close')} />
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+            <p>Login - Under development</p>
+          </IonModal>
         </IonContent>
       </IonPage>
     );
@@ -25,4 +82,4 @@ class Login extends React.Component {
 
 Login.contextType = AppContext;
 
-export default Login;
+export default withRouter(Login);
