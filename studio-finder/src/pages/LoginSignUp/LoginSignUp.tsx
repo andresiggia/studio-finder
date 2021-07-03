858b2d9aef92161741de33443991a01d47d9d263
Andre Martins <asiggi01@dcs.bbk.ac.uk>
2021-07-03 18:11:13 +0100

    login logic

--- studio-finder/src/pages/LoginSignUp/LoginSignUp.tsx
+++ studio-finder/src/pages/LoginSignUp/LoginSignUp.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import {
   IonContent, IonPage, IonModal, IonToolbar, IonIcon, IonButton, IonButtons, IonSegment,
-  IonSegmentButton, IonLabel, IonList, IonItem, IonInput, IonTitle,
+  IonSegmentButton, IonLabel, IonList, IonItem, IonInput, IonTitle, IonSpinner, IonToast,
 } from '@ionic/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { closeOutline } from 'ionicons/icons';
@@ -25,6 +25,8 @@ interface State {
   email: string;
   password: string;
   passwordRepeat: string;
+  isLoading: boolean,
+  error: Error | null,
 }
 
 class LoginSignUp extends React.Component<RouteComponentProps, State> {
@@ -37,6 +39,8 @@ class LoginSignUp extends React.Component<RouteComponentProps, State> {
       email: '',
       password: '',
       passwordRepeat: '',
+      isLoading: false,
+      error: null,
     };
   }
 
@@ -75,62 +79,155 @@ class LoginSignUp extends React.Component<RouteComponentProps, State> {
     history.push(`${location.pathname}?${query.toString()}`);
   }
 
-  onSubmit = () => {
-    // to do
+  onSubmit = (e: any) => {
+    // prevent form from submitting
+    e.preventDefault();
+    const isValidForm = this.isValidForm();
+    if (isValidForm) {
+      this.setState({
+        isLoading: true,
+      }, async () => {
+        const screen = this.getScreen();
+        try {
+          const { email, password } = this.state;
+          const { auth } = this.context;
+          if (screen === RouteNames.signUp) {
+            const { error: signUpError } = await auth.signUp({
+              email,
+              password,
+            });
+            if (signUpError) {
+              throw signUpError;
+            }
+          } else {
+            const { error: signInError } = await auth.signIn({
+              email,
+              password,
+            });
+            if (signInError) {
+              throw signInError;
+            }
+          }
+          this.setState({
+            isLoading: false,
+          }, () => this.onClose);
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.warn('error - onSubmit', error);
+          let errorMsg = (screen === RouteNames.signUp)
+            ? i18n.t('An error occurred during sign up')
+            : i18n.t('An error occurred during login');
+          switch (error?.status) {
+            case 400:
+              errorMsg = i18n.t('Invalid email or password');
+              break;
+            default:
+          }
+          this.setState({
+            isLoading: false,
+            error: new Error(errorMsg),
+          });
+        }
+      });
+    }
+  }
+
+  isValidForm = () => {
+    const { email, password, passwordRepeat } = this.state;
+    const screen = this.getScreen();
+    const MIN_PASSWORD_CHARS = 4;
+    return !!email && !!password && password.length >= MIN_PASSWORD_CHARS
+      && (screen !== RouteNames.signUp || (!!passwordRepeat && password === passwordRepeat));
   }
 
   // render
 
   renderForm = () => {
     const {
-      email, password, passwordRepeat,
+      email, password, passwordRepeat, isLoading, error,
     } = this.state;
     const screen = this.getScreen();
+    const isValidForm = this.isValidForm();
     return (
       <form onSubmit={this.onSubmit}>
-        <IonList className="login-form">
-          <IonItem>
-            <IonInput
-              value={email}
-              type="text"
-              required
-              placeholder={i18n.t('Email')}
-              onIonChange={(e) => {
-                this.setState({
-                  email: e.detail.value || '',
-                });
-              }}
-            />
-          </IonItem>
-          <IonItem>
-            <IonInput
-              value={password}
-              type="password"
-              required
-              placeholder={i18n.t('Password')}
-              onIonChange={(e) => {
-                this.setState({
-                  password: e.detail.value || '',
-                });
-              }}
-            />
-          </IonItem>
-          {screen === RouteNames.signUp && (
+        <fieldset disabled={isLoading || !!error}>
+          <IonList className="login-form">
             <IonItem>
               <IonInput
-                value={passwordRepeat}
-                type="password"
+                value={email}
+                type="text"
                 required
-                placeholder={i18n.t('Repeat password')}
+                placeholder={i18n.t('Email')}
                 onIonChange={(e) => {
                   this.setState({
-                    passwordRepeat: e.detail.value || '',
+                    email: e.detail.value || '',
                   });
                 }}
               />
             </IonItem>
-          )}
-        </IonList>
+            <IonItem>
+              <IonInput
+                value={password}
+                type="password"
+                required
+                placeholder={i18n.t('Password')}
+                onIonChange={(e) => {
+                  this.setState({
+                    password: e.detail.value || '',
+                  });
+                }}
+              />
+            </IonItem>
+            {screen === RouteNames.signUp && (
+              <IonItem>
+                <IonInput
+                  value={passwordRepeat}
+                  type="password"
+                  required
+                  placeholder={i18n.t('Repeat password')}
+                  onIonChange={(e) => {
+                    this.setState({
+                      passwordRepeat: e.detail.value || '',
+                    });
+                  }}
+                />
+              </IonItem>
+            )}
+            <IonItem>
+              <IonButton
+                color="primary"
+                type="submit"
+                disabled={!isValidForm}
+              >
+                {i18n.t('Confirm')}
+              </IonButton>
+            </IonItem>
+            <IonItem>
+              <IonButton
+                fill="outline"
+                type="reset"
+                disabled={!email && !password && !passwordRepeat}
+                onClick={this.onClose}
+              >
+                {i18n.t('Cancel')}
+              </IonButton>
+            </IonItem>
+            {isLoading && (
+              <IonItem>
+                <IonSpinner name="bubbles" />
+              </IonItem>
+            )}
+          </IonList>
+          <IonToast
+            isOpen={!!error}
+            onDidDismiss={() => this.setState({ error: null })}
+            header="Error"
+            position="bottom"
+            color="danger"
+            message={error?.message || i18n.t('An error occurred, please try again later')}
+            duration={3000}
+          />
+        </fieldset>
       </form>
     );
   }
