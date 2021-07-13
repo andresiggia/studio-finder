cdf12f39b186f3b75ba69799afe10dede89d52b6
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-07-13 16:16:57 +0100

    add method updateObjectKeysToUnderscoreCase to convert object back to DB

--- studio-finder/src/services/api/api.ts
+++ studio-finder/src/services/api/api.ts
@@ -3,6 +3,9 @@ export const toCamelCase = (str: string) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (
   index === 0 ? word.toLowerCase() : word.toUpperCase()
 )).replace(/\s+/g, '');
 
+// from https://stackoverflow.com/questions/30970286/convert-javascript-object-camelcase-keys-to-underscore-case
+export const toUnderscoreCase = (str: string) => str.replace(/([A-Z])/g, '_$1').toLowerCase();
+
 export const updateObjectKeysToCamelCase = (originalObj: any) => {
   let obj: any = null;
   if (originalObj) {
@@ -10,6 +13,18 @@ export const updateObjectKeysToCamelCase = (originalObj: any) => {
     Object.keys(originalObj).forEach((key: string) => {
       const ccKey = toCamelCase(key);
       obj[ccKey] = originalObj[key];
+    });
+  }
+  return obj;
+};
+
+export const updateObjectKeysToUnderscoreCase = (originalObj: any) => {
+  let obj: any = null;
+  if (originalObj) {
+    obj = {};
+    Object.keys(originalObj).forEach((key: string) => {
+      const ucKey = toUnderscoreCase(key);
+      obj[ucKey] = originalObj[key];
     });
   }
   return obj;
