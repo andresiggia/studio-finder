ae7f84ba6d88cbcb011a646d75432f0b8a24628b
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-07-13 14:38:31 +0100

    loading userProfile after updateUser

--- studio-finder/src/context/AppContext.tsx
+++ studio-finder/src/context/AppContext.tsx
@@ -5,10 +5,12 @@ import supabase, { createClient } from '@supabase/supabase-js';
 // constants
 import appKeys from '../constants/supabase-keys';
 import { USER_TYPES } from '../constants/user-types';
+import { TABLE_NAMES, UserProfile } from '../constants/tables';
 
 // services
 import { i18nInit } from '../services/i18n/i18n';
 import { getRoutesByName, RouteNames, LoginRouteNames } from '../services/routes/routes';
+import { updateObjectKeysToCamelCase } from '../services/api/api';
 
 const AppContext = React.createContext({});
 
@@ -25,6 +27,7 @@ interface AuthResponse {
 
 interface State {
   user: supabase.User | null,
+  profile: UserProfile | null,
 }
 
 export class AppContextProvider extends React.Component<Props, State> {
@@ -39,6 +42,7 @@ export class AppContextProvider extends React.Component<Props, State> {
 
     this.state = {
       user: this.supabase.auth.user(),
+      profile: null,
     };
 
     i18nInit();
@@ -70,10 +74,57 @@ export class AppContextProvider extends React.Component<Props, State> {
     }
   }
 
-  updateUser = async (user?: supabase.User | null) => new Promise((resolve) => {
-    this.setMountedState({
-      user: user || null,
-    }, () => resolve(true));
+  loadUserProfile = async () => {
+    try {
+      const { user } = this.state;
+      if (!user) {
+        // eslint-disable-next-line no-console
+        console.log('loading user profile...');
+        return Promise.resolve({ data: null });
+      }
+      // eslint-disable-next-line no-console
+      console.log('loading user profile...');
+      const { data, error } = await this.supabase
+        .from(TABLE_NAMES.users)
+        .select()
+        .match({
+          id: user.id,
+        });
+      if (error) {
+        throw error;
+      }
+      let profileData: any;
+      if (data && Array.isArray(data) && data.length > 0) {
+        [profileData] = data;
+      }
+      const profile: any = updateObjectKeysToCamelCase(profileData);
+      // eslint-disable-next-line no-console
+      console.log('got user profile', profile);
+      return new Promise((resolve) => {
+        this.setMountedState({
+          profile: profile || null,
+        }, () => resolve({ data: profile }));
+      });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn('error - loadUserProfile', error);
+      return Promise.resolve({ error });
+    }
+  }
+
+  updateUser = (user?: supabase.User | null) => new Promise((resolve) => {
+    const { user: currentUser } = this.state;
+    if (user === currentUser) {
+      resolve(true);
+    } else {
+      this.setMountedState({
+        user: user || null,
+        profile: null, // reset user profile when auth user changes
+      }, async () => {
+        await this.loadUserProfile();
+        resolve(true);
+      });
+    }
   })
 
   getLoginPath = (options: {
