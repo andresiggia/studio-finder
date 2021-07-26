6df5a61fcc8ee996d721f0fe5a8119d0939ae8fe
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-07-26 11:54:36 +0100

    updates to forgotPassword page / change password component

---
+++ studio-finder/src/components/ChangePasswordForm/ChangePasswordForm.tsx
@@ -0,0 +1,239 @@
+import React from 'react';
+import {
+  IonButton, IonCol, IonGrid, IonIcon, IonList, IonRow, IonSpinner,
+} from '@ionic/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { refreshOutline } from 'ionicons/icons';
+
+// context
+import AppContext from '../../context/AppContext';
+
+// components
+import Notification, { NotificationProps, NotificationType } from '../Notification/Notification';
+import PasswordFields from '../PasswordFields/PasswordFields';
+
+// services
+import i18n from '../../services/i18n/i18n';
+
+// constants
+import { MIN_PASSWORD_CHARS } from '../../constants/settings';
+
+// css
+import './ChangePasswordForm.css';
+
+interface Props {
+  onCancel?: () => void,
+}
+
+interface State {
+  password: string;
+  passwordRepeat: string;
+  isLoading: boolean,
+  error: Error | null,
+  notification: NotificationProps | null,
+}
+
+class ChangePasswordForm extends React.Component<Props, State> {
+  mounted = false
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      password: '',
+      passwordRepeat: '',
+      isLoading: false,
+      error: null,
+      notification: null,
+    };
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  setMountedState = (state: any, callback?: () => any) => {
+    if (this.mounted) {
+      this.setState(state, callback);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('unmounted request', state);
+      if (typeof callback === 'function') {
+        callback();
+      }
+    }
+  }
+
+  onSubmit = (e: any) => {
+    // prevent form from submitting
+    e.preventDefault();
+    const isValidForm = this.isValidForm();
+    if (isValidForm) {
+      this.setMountedState({
+        isLoading: true,
+      }, async () => {
+        try {
+          const { password } = this.state;
+          const { supabase, state } = this.context;
+          const { onCancel } = this.props;
+          let notification = null;
+          const { error } = await supabase.auth.api.updateUser(state.accessToken, { password });
+          if (error) {
+            throw error;
+          }
+          notification = {
+            header: i18n.t('Password changed successfully'),
+            message: '',
+            type: 'success',
+            onDismiss: () => {
+              if (typeof onCancel === 'function') {
+                onCancel();
+              } else {
+                this.setMountedState({
+                  notification: null,
+                });
+              }
+            },
+          } as NotificationProps;
+          this.setMountedState({
+            isLoading: false,
+            notification,
+          });
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.warn('error - onSubmit', error);
+          this.setMountedState({
+            isLoading: false,
+            error,
+          });
+        }
+      });
+    }
+  }
+
+  onReset = () => {
+    this.setMountedState({
+      password: '',
+      passwordRepeat: '',
+    });
+  }
+
+  isValidForm = () => {
+    const { password, passwordRepeat } = this.state;
+    const isPasswordValid = !!password && password.length >= MIN_PASSWORD_CHARS;
+    const isPasswordRepeatValid = !!passwordRepeat && password === passwordRepeat;
+    return isPasswordValid && isPasswordRepeatValid;
+  }
+
+  hasChanges = () => {
+    const { password, passwordRepeat } = this.state;
+    return !!password || !!passwordRepeat;
+  }
+
+  // render
+
+  render() {
+    const { onCancel } = this.props;
+    const {
+      password, passwordRepeat, isLoading, error, notification,
+    } = this.state;
+    const isValidForm = this.isValidForm();
+    const hasChanges = this.hasChanges();
+    const disabled = isLoading || !!error || !!notification;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <fieldset className="changepass-form-fieldset" disabled={disabled}>
+          <IonList className="changepass-form-list">
+            <PasswordFields
+              password={password}
+              onPasswordChange={(e) => {
+                this.setMountedState({
+                  password: e.detail.value || '',
+                });
+              }}
+              showPasswordRepeat
+              passwordRepeat={passwordRepeat}
+              onPasswordRepeatChange={(e) => {
+                this.setMountedState({
+                  passwordRepeat: e.detail.value || '',
+                });
+              }}
+              disabled={disabled}
+              notificationClassName="login-spacer"
+            />
+          </IonList>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12" size-md="6">
+                {(hasChanges || typeof onCancel !== 'function')
+                  ? (
+                    <IonButton
+                      fill="outline"
+                      type="button"
+                      expand="block"
+                      disabled={disabled || !hasChanges}
+                      onClick={() => this.onReset()}
+                    >
+                      <IonIcon slot="start" icon={refreshOutline} />
+                      {i18n.t('Reset')}
+                    </IonButton>
+                  ) : (
+                    <IonButton
+                      fill="outline"
+                      type="button"
+                      expand="block"
+                      disabled={disabled}
+                      onClick={() => onCancel()}
+                    >
+                      {i18n.t('Cancel')}
+                    </IonButton>
+                  )}
+              </IonCol>
+              <IonCol size="12" size-md="6">
+                <IonButton
+                  color="primary"
+                  type="submit"
+                  expand="block"
+                  disabled={disabled || !isValidForm}
+                >
+                  {i18n.t('Confirm')}
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+          {isLoading && (
+            <div className="changepass-loading changepass-spacer">
+              <IonSpinner name="bubbles" />
+            </div>
+          )}
+          {!!error && (
+            <Notification
+              type={NotificationType.danger}
+              className="changepass-spacer"
+              header={i18n.t('Error')}
+              message={error?.message || i18n.t('An error occurred, please try again later')}
+              onDismiss={() => this.setMountedState({ error: null })}
+            />
+          )}
+          {!!notification && (
+            <Notification
+              type={notification?.type}
+              className="changepass-spacer"
+              header={notification?.header}
+              message={notification?.message}
+              preventDismiss={notification?.preventDismiss}
+              onDismiss={() => this.setMountedState({ notification: null })}
+            />
+          )}
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+ChangePasswordForm.contextType = AppContext;
+
+export default ChangePasswordForm;
--- studio-finder/src/pages/ForgotPassword/ForgotPassword.tsx
+++ studio-finder/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import {
+  IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonPage, IonRow,
+} from '@ionic/react';
+import { RouteComponentProps } from 'react-router';
 
 // services
 import i18n from '../../services/i18n/i18n';
 
+// context
+import AppContext from '../../context/AppContext';
+
 // components
 import Header from '../../components/Header/Header';
 import Notification, { NotificationType } from '../../components/Notification/Notification';
-import ForgotPasswordForm from '../../components/ForgotPasswordForm/ForgotPasswordForm';
+import ChangePasswordForm from '../../components/ChangePasswordForm/ChangePasswordForm';
+import { getRoutesByName, RouteNames } from '../../services/routes/routes';
 
 // css
 // import './ForgotPassword.css';
 
-class ForgotPassword extends React.Component {
+class ForgotPassword extends React.Component<RouteComponentProps> {
   getHashParams = () => {
     const { hash } = window.location;
     if (hash && hash.includes('#')) {
@@ -29,20 +36,37 @@ class ForgotPassword extends React.Component {
     return null;
   }
 
+  onCancel = () => {
+    const { history } = this.props;
+    const [route] = getRoutesByName([RouteNames.account]);
+    history.push(route.path);
+  }
+
+  // render
+
   renderForm = () => {
+    const { state } = this.context;
     const params = this.getHashParams();
-    if (params?.access_token && params?.type === 'recovery') {
+    if (state.user || (params?.access_token && params?.type === 'recovery')) {
       return (
-        <ForgotPasswordForm
-          accessToken={params.access_token}
-        />
+        <ChangePasswordForm onCancel={() => this.onCancel()} />
       );
+    }
+    let title = i18n.t('Error');
+    let message = i18n.t('Missing token to redefine password, please try again later.');
+    if (params?.error_code) {
+      message = i18n.t('An error occurred while requesting to reset your password, please try again later.');
+      if (params.error_code === '404') {
+        message = i18n.t('User not found');
+      } else {
+        title = `${i18n.t('Error')} ${params.error_code}`;
+      }
     }
     return (
       <Notification
         type={NotificationType.danger}
-        header={i18n.t('Error')}
-        message={i18n.t('Missing token to redefine password, please try again later.')}
+        header={title}
+        message={message}
         preventDismiss
       />
     );
@@ -53,11 +77,28 @@ class ForgotPassword extends React.Component {
       <IonPage>
         <IonContent fullscreen>
           <Header />
-          {this.renderForm()}
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12" size-lg="4" offset-lg="4" size-md="6" offset-md="3">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>
+                      {i18n.t('Redefine Password')}
+                    </IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    {this.renderForm()}
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
         </IonContent>
       </IonPage>
     );
   }
 }
+
+ForgotPassword.contextType = AppContext;
 
 export default ForgotPassword;
