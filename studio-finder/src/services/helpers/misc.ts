7a6399c50e58ad95ed6aded395ace2355f71e44b
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-07-29 10:25:38 +0100

    fix for deepEqual issue

--- studio-finder/src/services/helpers/misc.ts
+++ studio-finder/src/services/helpers/misc.ts
@@ -1,6 +1,8 @@
 // adapted from https://dmitripavlutin.com/how-to-compare-objects-in-javascript/
 export const isObject = (item: any) => item != null && typeof item === 'object';
 
+export const isDateEqual = (val1: Date, val2: Date) => val1.getTime() === val2.getTime();
+
 // adapted from https://dmitripavlutin.com/how-to-compare-objects-in-javascript/
 export const deepEqual = (item1: any, item2: any): boolean => {
   const isObject1 = isObject(item1);
@@ -20,21 +22,19 @@ export const deepEqual = (item1: any, item2: any): boolean => {
   const keys1 = Object.keys(item1);
   const keys2 = Object.keys(item2);
 
+  if (item1 instanceof Date && item2 instanceof Date) {
+    return isDateEqual(item1, item2);
+  }
+
   // if number of properties differ
   if (keys1.length !== keys2.length) {
     return false;
   }
 
   // compare properties
-  return Object.keys(keys1).every((key: string) => {
+  return keys1.every((key: string) => {
     const val1 = item1[key];
     const val2 = item2[key];
-    if (val1 instanceof Date && val2 instanceof Date) {
-      return val1.getTime() === val2.getTime();
-    }
-    const areObjects = isObject(val1) && isObject(val2);
-    return areObjects
-      ? deepEqual(val1, val2)
-      : val1 === val2;
+    return deepEqual(val1, val2);
   });
 };
