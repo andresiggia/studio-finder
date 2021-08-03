4400cbc99796eb0e2fe66c8ad570f6aaf74f3abc
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-03 11:00:12 +0100

    update BookingItemList to show calendar

--- studio-finder/src/components/BookingItemList/BookingItemList.tsx
+++ studio-finder/src/components/BookingItemList/BookingItemList.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {
-  IonButton, IonButtons, IonCol, IonContent, IonGrid, IonIcon, IonItem, IonLabel, IonList, IonModal, IonRow,
   IonSpinner, IonTitle, IonToolbar,
 } from '@ionic/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { addOutline, closeOutline, createOutline } from 'ionicons/icons';
+// import { closeOutline } from 'ionicons/icons';
 
 // services
 import { SpaceProfile } from '../../services/api/spaces';
 import i18n from '../../services/i18n/i18n';
-import { getBookingItems } from '../../services/api/bookings';
+import { BookingItem, getBookingItems } from '../../services/api/bookings';
+import { pad } from '../../services/helpers/misc';
 
 // components
 import Notification, { NotificationType } from '../Notification/Notification';
@@ -24,10 +24,7 @@ import './BookingItemList.css';
 interface State {
   isLoading: boolean,
   error: Error | null,
-  items: any[] | null,
-  // selectedId: number,
-  showModal: boolean,
-  modalSelectedId: number,
+  items: BookingItem[] | null,
 }
 
 interface Props {
@@ -43,9 +40,6 @@ class BookingItemList extends React.Component<Props, State> {
       isLoading: false,
       error: null,
       items: null,
-      // selectedId: 0,
-      showModal: false,
-      modalSelectedId: 0,
     };
   }
 
@@ -62,9 +56,6 @@ class BookingItemList extends React.Component<Props, State> {
   }
 
   componentWillUnmount() {
-    this.setMountedState({
-      showModal: false,
-    });
     this.mounted = false;
   }
 
@@ -108,155 +99,101 @@ class BookingItemList extends React.Component<Props, State> {
     });
   }
 
-  onModalOpen = (modalSelectedId = 0) => {
-    this.setMountedState({
-      showModal: true,
-      modalSelectedId,
-    });
-  }
-
-  onModalClose = () => {
-    this.setMountedState({
-      showModal: false,
-    });
-  }
-
   // render
 
-  // renderSelectedBookingItem = () => {
-  //   const { items, selectedId } = this.state;
-  //   const bookingItemProfile = items?.find((item) => item.id === selectedId);
-  //   if (!bookingItemProfile) {
-  //     return (
-  //       <Notification
-  //         type={NotificationType.danger}
-  //         className="booking-item-list-bookingItemr"
-  //         header={i18n.t('Not found')}
-  //         message={i18n.t('Invalid selected BookingItem')}
-  //         onDismiss={() => this.setMountedState({ selectedId: 0 })}
-  //       />
-  //     );
-  //   }
-  //   return (
-  //     <div className="booking-item-list-item-container">
-  //       <IonToolbar>
-  //         <IonTitle size="small" className="booking-item-list-item-title">
-  //           {bookingItemProfile.title}
-  //         </IonTitle>
-  //         <IonButtons slot="end" className="booking-item-list-item-toolbar">
-  //           <IonButton
-  //             fill="outline"
-  //             color="primary"
-  //             title={i18n.t('Edit BookingItem')}
-  //             onClick={() => this.onModalOpen(selectedId)}
-  //           >
-  //             <IonIcon icon={createOutline} />
-  //           </IonButton>
-  //         </IonButtons>
-  //       </IonToolbar>
-  //       <p>{bookingItemProfile.description || `(${i18n.t('No description')})`}</p>
-  //     </div>
-  //   );
-  // }
-
-  renderModalBookingItem = () => {
-    // const { spaceProfile } = this.props;
-    const { showModal, modalSelectedId } = this.state;
+  renderCalendar = () => {
+    const { items } = this.state;
+    const weekdays = Array.from(Array(7)).map((_item, index) => index);
+    const now = new Date();
+    const currentWeekday = now.getDay();
+    const weekStartsAt = new Date(now.getTime());
+    weekStartsAt.setDate(weekStartsAt.getDate() - currentWeekday);
+    const halfTimes = Array.from(Array(24 * 2)).map((_item, index) => index / 2);
     return (
-      <IonModal
-        isOpen={showModal}
-        onWillDismiss={() => this.onModalClose()}
-      >
-        <IonToolbar>
-          <IonTitle>
-            {modalSelectedId
-              ? i18n.t('Edit BookingItem')
-              : i18n.t('Create BookingItem')}
-          </IonTitle>
-          <IonButtons slot="end">
-            <IonButton
-              color="primary"
-              onClick={() => this.onModalClose()}
-            >
-              <IonIcon icon={closeOutline} ariaLabel={i18n.t('Close')} />
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-        <IonContent>
-          {showModal && (
-            <p>Under development</p>
-            // <BookingItemForm
-            //   id={modalSelectedId}
-            //   spaceProfile={spaceProfile}
-            //   onCancel={() => this.onModalClose()}
-            //   onSave={() => {
-            //     this.onModalClose();
-            //     this.loadItems();
-            //   }}
-            // />
-          )}
-        </IonContent>
-      </IonModal>
+      <div className="booking-item-calendar-container">
+        <table className="booking-item-calendar">
+          <thead>
+            <tr>
+              <td className="booking-item-calendar-first-col">
+                {i18n.t('Time')}
+              </td>
+              {weekdays.map((weekday) => {
+                const date = new Date(weekStartsAt.getTime());
+                date.setDate(date.getDate() + weekday);
+                return (
+                  <td key={weekday} className="booking-item-calendar-col">
+                    {date.toLocaleDateString()}
+                  </td>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {halfTimes.map((time) => {
+              const timeArr = String(time).split('.');
+              const hours = Number(timeArr[0]);
+              const minutes = (Number(timeArr[1] || 0) / 10) * 60;
+              return (
+                <tr key={time}>
+                  <td className="booking-item-calendar-first-col">
+                    <p>{`${pad(hours, 2)}:${pad(minutes, 2)}`}</p>
+                  </td>
+                  {weekdays.map((weekday) => {
+                    const date = new Date(weekStartsAt.getTime());
+                    date.setDate(date.getDate() + weekday);
+                    date.setHours(hours, minutes);
+                    const relevantItems = (items || []).filter((item) => {
+                      if (!item.startAt || !item.endAt) {
+                        return false;
+                      }
+                      const currentTimestamp = date.getTime();
+                      const startAtTimestamp = item.startAt.getTime();
+                      const endAtTimestamp = item.endAt.getTime();
+                      return startAtTimestamp >= currentTimestamp && endAtTimestamp <= currentTimestamp;
+                    });
+                    return (
+                      <td key={weekday} className="booking-item-calendar-col">
+                        {relevantItems.map((item) => (
+                          <p key={item.bookingId}>
+                            {item.serviceTitle}
+                          </p>
+                        ))}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 
   renderContent = () => {
-    const {
-      isLoading, error, items, // selectedId,
-    } = this.state;
-    return (
-      <>
-        {isLoading && (
-          <div className="booking-item-list-loading booking-item-list-bookingItemr">
-            <IonSpinner name="bubbles" />
-          </div>
-        )}
-        {!!error && (
-          <Notification
-            type={NotificationType.danger}
-            className="booking-item-list-bookingItemr"
-            header={i18n.t('Error')}
-            message={error?.message || i18n.t('An error occurred, please try again later')}
-            onDismiss={() => this.setMountedState({ error: null })}
-          />
-        )}
-        <IonGrid>
-          <IonRow>
-            <IonCol size="12" size-md="6" size-lg="4">
-              {!items || items.length === 0
-                ? (
-                  <p>{i18n.t('No bookings found.')}</p>
-                ) : (
-                  <IonList className="booking-item-list-items">
-                    {items.map((item, i) => (
-                      <IonItem
-                        // eslint-disable-next-line react/no-array-index-key
-                        key={i}
-                        detail
-                        button
-                        // color={item.id === selectedId
-                        //   ? 'primary'
-                        //   : ''}
-                        onClick={() => this.setMountedState({ selectedId: item.id })}
-                        title={item.title}
-                      >
-                        <IonLabel>
-                          {item.title}
-                        </IonLabel>
-                      </IonItem>
-                    ))}
-                  </IonList>
-                )}
-            </IonCol>
-            <IonCol size="12" size-md="6" size-lg="8">
-              {/* {!!selectedId && (
-                this.renderSelectedBookingItem()
-              )} */}
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </>
-    );
+    const { isLoading, error } = this.state;
+
+    if (isLoading) {
+      return (
+        <div className="booking-item-list-loading booking-item-list-spacer">
+          <IonSpinner name="bubbles" />
+        </div>
+      );
+    }
+
+    if (error) {
+      return (
+        <Notification
+          type={NotificationType.danger}
+          className="booking-item-list-spacer"
+          header={i18n.t('Error')}
+          message={error?.message || i18n.t('An error occurred, please try again later')}
+          onDismiss={() => this.setMountedState({ error: null })}
+        />
+      );
+    }
+
+    return this.renderCalendar();
   }
 
   render() {
@@ -264,22 +201,10 @@ class BookingItemList extends React.Component<Props, State> {
       <>
         <IonToolbar>
           <IonTitle size="small" className="booking-item-list-title">
-            {i18n.t('Bookings')}
+            {i18n.t('Booking Calendar')}
           </IonTitle>
-          {/* <IonButtons slot="end">
-            <IonButton
-              fill="outline"
-              color="primary"
-              title={i18n.t('Add BookingItem')}
-              onClick={() => this.onModalOpen()}
-            >
-              <IonIcon icon={addOutline} />
-              {i18n.t('BookingItem')}
-            </IonButton>
-          </IonButtons> */}
         </IonToolbar>
         {this.renderContent()}
-        {this.renderModalBookingItem()}
       </>
     );
   }
