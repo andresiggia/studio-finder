b17d9580e863c68cd9ce36e36f53c949945eb384
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-12 10:31:45 +0100

    showing serviceTitle only when avaialble

--- studio-finder/src/components/BookingItemList/BookingItemList.tsx
+++ studio-finder/src/components/BookingItemList/BookingItemList.tsx
@@ -111,7 +111,10 @@ class BookingItemList extends React.Component<Props, State> {
     return (
       <IonList className="booking-item-list-items">
         {items.map((item, index) => {
-          const label = `${index + 1}. ${item.spaceTitle} (${item.serviceTitle})`;
+          const label = `${index + 1}. ${item.spaceTitle}${
+            item.serviceTitle
+              ? ` (${item.serviceTitle})`
+              : ''}`;
           return (
             <IonItem
               // eslint-disable-next-line react/no-array-index-key
