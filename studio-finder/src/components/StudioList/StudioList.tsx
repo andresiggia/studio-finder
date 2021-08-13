0ffa8faaecd9a12312e0d0478c0602b50f4f9253
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-13 17:45:56 +0100

    refactorr

--- studio-finder/src/components/StudioList/StudioList.tsx
+++ studio-finder/src/components/StudioList/StudioList.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardTitle,
-  IonCol, IonGrid, IonIcon, IonRow, IonSpinner,
+  IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonIcon, IonRow, IonSpinner,
 } from '@ionic/react';
 import {
   withRouter, RouteComponentProps, generatePath,
