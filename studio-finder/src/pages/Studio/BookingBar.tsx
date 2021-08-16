adfdbebe87f424e556d39fad5c8b6017b3de3b6f
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-16 17:33:49 +0100

    group booking items

--- studio-finder/src/pages/Studio/BookingBar.tsx
+++ studio-finder/src/pages/Studio/BookingBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  IonCol, IonGrid, IonIcon, IonItem, IonLabel, IonList, IonRow,
+  IonCol, IonGrid, IonIcon, IonItem, IonLabel, IonList, IonRow, IonText,
 } from '@ionic/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {
@@ -14,7 +14,7 @@ import AppContext from '../../context/AppContext';
 import i18n from '../../services/i18n/i18n';
 import { SpaceProfile } from '../../services/api/spaces';
 import { StudioProfile } from '../../services/api/studios';
-import { BookingDate } from '../../services/api/bookingItems';
+import { BookingDate, BookingItem, defaultBookingItem } from '../../services/api/bookingItems';
 import { sortByKey } from '../../services/helpers/misc';
 import { Service } from '../../services/api/services';
 
@@ -35,43 +35,82 @@ class BookingBar extends React.Component<Props> {
     const weekdayFormat = new Intl.DateTimeFormat(i18n.languages, {
       weekday: 'short',
     });
-    const dateTimeFormat = new Intl.DateTimeFormat(i18n.languages, {
-      dateStyle: 'medium',
+    const dateFormat = new Intl.DateTimeFormat(i18n.languages, {
+      dateStyle: 'long',
+    });
+    const timeFormat = new Intl.DateTimeFormat(i18n.languages, {
       timeStyle: 'short',
     });
     const sortedItems = sortByKey(bookingDates.map((item) => ({
       ...item,
       timestamp: item.date.getTime(),
     })), 'timestamp');
-    // let bookingItems = [];
-    // sortedItems.forEach((item) => {
-
-    // });
+    const DEFAULT_PERIOD_MODULE_MS = 1000 * 60 * 30; // 30min
+    const bookingItems: BookingItem[] = [];
+    sortedItems.forEach((item) => {
+      const index = bookingItems.findIndex((bItem) => (
+        item.spaceId === bItem.spaceId
+          && item.serviceType === bItem.serviceType
+          && bItem.endAt?.getTime() === item.date.getTime()
+      ));
+      const endAt = new Date(item.date.getTime() + DEFAULT_PERIOD_MODULE_MS);
+      if (index === -1) {
+        bookingItems.push({
+          ...defaultBookingItem,
+          spaceId: item.spaceId,
+          serviceType: item.serviceType,
+          startAt: item.date,
+          endAt,
+        });
+      } else {
+        bookingItems[index] = {
+          ...bookingItems[index],
+          endAt,
+        };
+      }
+    });
     return (
       <div className="booking-bar">
         <IonGrid className="booking-bar-container">
           <IonRow>
-            <IonCol size="12" size-md="6">
+            <IonCol size="12" size-md="6" size-lg="8">
               <IonLabel className="booking-bar-label">
                 {i18n.t('Booking selection')}
               </IonLabel>
               <IonList className="booking-bar-list">
                 {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-                {sortedItems.map(({ timestamp, ...item }, index) => {
-                  const space = spaces.find((spaceItem) => spaceItem.id === item.spaceId);
-                  const service = state.services?.find((serviceItem: Service) => serviceItem.type === item.serviceType);
+                {bookingItems.map((bookingItem, index) => {
+                  const space = spaces.find((spaceItem) => spaceItem.id === bookingItem.spaceId);
+                  const service = state.services?.find((serviceItem: Service) => serviceItem.type === bookingItem.serviceType);
+                  let dateTimeLabel = '';
+                  let totalHourslabel = '';
+                  if (bookingItem.startAt && bookingItem.endAt) {
+                    const startTimeStr = timeFormat.format(bookingItem.startAt);
+                    const endTimeStr = timeFormat.format(bookingItem.endAt);
+                    const startDateStr = `(${weekdayFormat.format(bookingItem.startAt)}) ${dateFormat.format(bookingItem.startAt)}`;
+                    const endDateStr = `(${weekdayFormat.format(bookingItem.endAt)}) ${dateFormat.format(bookingItem.endAt)}`;
+                    if (startDateStr === endDateStr) { // same date, different times
+                      dateTimeLabel = `${startDateStr} ${startTimeStr} - ${endTimeStr}`;
+                    } else { // different dates and times
+                      dateTimeLabel = `${startDateStr} ${startTimeStr} - ${endDateStr} ${endTimeStr}`;
+                    }
+                    const totalHours = (bookingItem.endAt.getTime() - bookingItem.startAt.getTime()) / (1000 * 60 * 60);
+                    totalHourslabel = `(${i18n.t('{{totalHours}} hours', { totalHours })})`;
+                  }
+                  const serviceLabel = `${service?.title || `${i18n.t('Service')} ${bookingItem.serviceType}`}`;
+                  const spaceLabel = `@${space?.title || `${i18n.t('Space')} ${bookingItem.spaceId}`}`;
+                  const mainLabel = `${serviceLabel} ${spaceLabel}`;
                   return (
                     // eslint-disable-next-line react/no-array-index-key
                     <IonItem key={index}>
-                      <strong>
-                        {`${
-                          service?.title || `${i18n.t('Service')} ${item.serviceType}`
-                        } @${space?.title || `${i18n.t('Space')} ${item.spaceId}`}`}
-                      </strong>
-                      <span>&nbsp;|&nbsp;</span>
-                      <span>
-                        {`(${weekdayFormat.format(item.date)}) ${dateTimeFormat.format(item.date)}`}
-                      </span>
+                      <IonLabel title={`${mainLabel} | ${dateTimeLabel} ${totalHourslabel}`}>
+                        <strong>{mainLabel}</strong>
+                        &nbsp;|&nbsp;
+                        {dateTimeLabel}
+                        <IonText color="medium">
+                          {` ${totalHourslabel}`}
+                        </IonText>
+                      </IonLabel>
                       <IonIcon
                         icon={closeCircle}
                         color="danger"
