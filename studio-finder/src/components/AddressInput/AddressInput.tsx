c2a6a2a1b85e4e0efb4f154ab4efa9b80c2ee3cf
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-18 17:01:02 +0100

    fix incorrect import

--- studio-finder/src/components/AddressInput/AddressInput.tsx
+++ studio-finder/src/components/AddressInput/AddressInput.tsx
@@ -5,7 +5,7 @@ import {
 
 // services
 import i18n from '../../services/i18n/i18n';
-import { geocode } from '../../services/geoapify/geocode';
+import { geocodeAddress } from '../../services/geoapify/geocode';
 import { Address, defaultAddress } from '../../services/api/address';
 
 // components
