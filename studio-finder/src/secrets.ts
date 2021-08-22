052340673f165304e6708da6809e2275991edb1c
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-22 13:13:28 +0100

    add production (restrricted) api key and use local env for dev key

--- studio-finder/src/secrets.ts
+++ studio-finder/src/secrets.ts
@@ -1,2 +1,2 @@
-// save real key only locally
-export const googleAPIKey = 'ABC123';
+export const googleAPIKey = process.env.GOOGLE_API_KEY // local development
+  || 'AIzaSyCfE5rk7byWYSjmd_rzSJRj2rDcvROciD4'; // production
