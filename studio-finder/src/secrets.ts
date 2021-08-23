974fef5f19a88c19b3cb0e1cdf9d6d52f1abfdd8
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-23 11:55:01 +0100

    fallback empty strring value

--- studio-finder/src/secrets.ts
+++ studio-finder/src/secrets.ts
@@ -1,4 +1,4 @@
-const googleAPIKeyDev = process.env.REACT_APP_GOOGLE_API_KEY;
+const googleAPIKeyDev = process.env.REACT_APP_GOOGLE_API_KEY || '';
 const googleAPIKeyProd = 'AIzaSyCfE5rk7byWYSjmd_rzSJRj2rDcvROciD4';
 
 export const googleAPIKey = (process.env.NODE_ENV !== 'production')
