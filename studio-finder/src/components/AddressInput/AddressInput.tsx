bba7ea51cafba36a7c8767bfb5e2dd78de42f9d8
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-08-23 11:13:13 +0100

    using usePlacesService for more control over api service

--- studio-finder/src/components/AddressInput/AddressInput.tsx
+++ studio-finder/src/components/AddressInput/AddressInput.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  IonInput,
+  IonInput, IonItem, IonList, IonSpinner, IonText,
 } from '@ionic/react';
-import AutoComplete from 'react-google-autocomplete';
+import usePlacesService from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
 
 // services
 import i18n from '../../services/i18n/i18n';
@@ -23,58 +23,77 @@ interface Props {
   onChange: (address: Address) => void,
 }
 
-class AddressInput extends React.Component<Props> {
-  render() {
-    const {
-      value, placeholder, disabled = false, required = false, color, onChange,
-    } = this.props;
-    return (
-      <div
-        className={`address-input ${
-          value ? '' : 'sc-ion-input-md-h sc-ion-input-md-s md'
-        }`}
-      >
-        {value
-          ? (
-            <IonInput
-              value={value}
-              type="text"
-              disabled={disabled}
-              required={required}
-              placeholder={placeholder}
-              clearInput={!!value}
-              clearOnEdit={!!value}
-              color={color}
-              onIonInput={() => {
-                onChange(defaultAddress);
-              }}
-              onIonChange={(e: any) => {
-                if (!e.detail.value) { // input was cleared
-                  onChange(defaultAddress);
-                }
-              }}
-            />
-          ) : (
-            <AutoComplete
-              apiKey={googleAPIKey}
-              className="native-input sc-ion-input-md"
-              options={{
-                types: ['address'],
-                componentRestrictions: { country: 'gb' },
-              }}
-              language={i18n.language}
-              placeholder={placeholder}
-              onPlaceSelected={(place) => {
-                const address = convertToAddress(place);
-                // eslint-disable-next-line no-console
-                console.log('got address', address, 'from', place);
-                onChange(address);
-              }}
-            />
-          )}
-      </div>
-    );
-  }
-}
+const AddressInput: React.FC<Props> = ({
+  value, placeholder, disabled = false, required = false, color, minChars = 3, onChange,
+}) => {
+  const [queryString, setQueryString] = useState('');
+  const {
+    placesService, getPlacePredictions, autocompleteSessionToken, isPlacePredictionsLoading, placePredictions,
+  } = usePlacesService({
+    apiKey: googleAPIKey,
+    language: i18n.language,
+  });
+  return (
+    <div className="address-input">
+      <IonInput
+        value={value || queryString}
+        type="text"
+        disabled={disabled}
+        required={required}
+        clearInput={!!value}
+        clearOnEdit={!!value}
+        color={color}
+        placeholder={placeholder}
+        onIonChange={(e: any) => {
+          const newValue = e.detail.value;
+          setQueryString(newValue);
+          if (!value) { // input was updated
+            if (newValue.length >= minChars) {
+              getPlacePredictions({
+                input: queryString,
+                componentRestrictions: { country: 'GB' },
+                sessionToken: autocompleteSessionToken,
+              });
+            }
+          } else if (!newValue) { // input was cleared
+            onChange(defaultAddress);
+          }
+        }}
+      />
+      {isPlacePredictionsLoading && (
+        <div className="address-input-loading">
+          <IonSpinner name="bubbles" />
+        </div>
+      )}
+      {(!value && !!placePredictions && !isPlacePredictionsLoading && queryString.length >= minChars) && (
+        <IonList className="address-input-results">
+          {placePredictions.length === 0
+            ? (
+              <IonText color="medium" className="address-input-results-none">
+                {`(${i18n.t('No results')})`}
+              </IonText>
+            ) : placePredictions.map((prediction) => (
+              <IonItem
+                key={prediction.place_id}
+                button
+                onClick={() => placesService?.getDetails({
+                  placeId: prediction.place_id,
+                }, (placeDetails: any) => {
+                  // eslint-disable-next-line no-console
+                  console.log('place details', placeDetails);
+                  const address = convertToAddress(placeDetails);
+                  setQueryString('');
+                  onChange(address);
+                })}
+              >
+                {prediction.description}
+              </IonItem>
+            ))}
+        </IonList>
+      )}
+    </div>
+  );
+  // }
+};
 
 export default AddressInput;
