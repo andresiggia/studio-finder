e3ccb0c147fceaad432049a8cfe38d3401a715c8
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-09-08 20:09:36 +0100

    fix for unit test unmounting before finishing

--- studio-finder/src/App.test.tsx
+++ studio-finder/src/App.test.tsx
@@ -4,12 +4,11 @@ import ReactDOM from 'react-dom';
 import { AppContextProvider } from './context/AppContext';
 import App from './App';
 
-it('renders without crashing', () => {
+it('renders without crashing', async () => {
   const div = document.createElement('div');
   ReactDOM.render((
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  ), div);
-  ReactDOM.unmountComponentAtNode(div);
+  ), div, () => ReactDOM.unmountComponentAtNode(div));
 });
