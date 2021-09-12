1c2bf44bd5bc00bfe502b396343cec1aab40d1ae
Andre Siggia <asiggi01@dcs.bbk.ac.uk>
2021-09-12 18:13:38 +0100

    remove city/country from Address

--- studio-finder/src/services/api/address.ts
+++ studio-finder/src/services/api/address.ts
@@ -1,15 +1,11 @@
 export interface Address {
   address: string,
-  city: string,
-  country: string,
   latitude: number,
   longitude: number,
 }
 
 export const defaultAddress: Address = {
   address: '',
-  city: '',
-  country: '',
   latitude: 0,
   longitude: 0,
 };
